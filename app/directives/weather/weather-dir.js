f6f6cfb113c42411f924bd77f3c31555d47434d8
Tyler Gregory <tyler.gregory@dealer.com>
2016-01-16 12:06:48 -0500

    master: switch between preset cities and show forecast

--- app/directives/weather/weather-dir.js
+++ app/directives/weather/weather-dir.js
@@ -4,16 +4,40 @@ angular.module('Guardian')
             restrict: 'E',
             replace: 'true',
             templateUrl: 'app/directives/weather/weather-dir.html',
+
             link: function ($scope, elem, attrs) {
-                var weather = weatherService.getCurrentWeatherByCity('5243008');
-                weather.then(function (answer) {
-                    $scope.weather = answer.data;
+                $scope.cityId = '5243008';
+
+                $scope.makeIconSrc = function makeIconSrc(icon) {
+                    return makeIconSrc(icon);
+                };
+
+                $scope.$watch('cityId', function (newvalue, oldvalue) {
+                    getCurrentWeather($scope, weatherService);
+                    getForecast($scope, weatherService);
                 });
 
-                var forecast = weatherService.getForecastByCity('5243008');
-                forecast.then(function (answer) {
-                    $scope.forecast = answer.data;
-                })
+                getCurrentWeather($scope, weatherService);
+                getForecast($scope, weatherService);
             }
         };
     });
+
+function makeIconSrc(icon) {
+    return "http://openweathermap.org/img/w/" + icon + ".png";
+}
+
+function getCurrentWeather($scope, weatherService) {
+    var weather = weatherService.getCurrentWeatherByCity($scope.cityId);
+    weather.then(function (answer) {
+        $scope.weather = answer.data;
+        $scope.iconSrc = makeIconSrc(answer.data.weather[0].icon);
+    });
+}
+
+function getForecast($scope, weatherService) {
+    var forecast = weatherService.getForecastByCity($scope.cityId);
+    forecast.then(function (answer) {
+        $scope.forecast = answer.data;
+    })
+}
